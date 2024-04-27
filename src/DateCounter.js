6b22d577e11f27a29499f567f739089a87806437
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-04-28 00:13:39 +0100

    okay

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,9 +1,24 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 function reducer(state, action) {
   console.log(state, action);
-  if (action.type === "inc") return state + 1;
-  if (action.type === "dec") return state - 1;
-  if (action.type === "setCount") return action.payload;
+
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return { ...state, count: 0, step: 1 };
+    default:
+      throw new Error("unknown action");
+  }
+  // if (action.type === "inc") return state + 1;
+  // if (action.type === "dec") return state - 1;
+  // if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
@@ -35,10 +50,12 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
     //setStep(Number(e.target.value));
   };
 
   const reset = function () {
+    dispatch({ type: "reset" });
     //setCount(0);
     // setStep(1);
   };
