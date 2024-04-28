af97bfad17213800da69c551e9e12643e5b8c7db
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-04-28 22:16:34 +0100

    okay

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+const initialState = {
+  questions: [],
+  status: "loading", //"loading", "ready", "error" "active" "finished"
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_QUESTIONS":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "setLoading":
+      return { ...state, status: "loading" };
+    case "setError":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("unknown action");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("https://opentdb.com/api.php?amount=15").then((res) =>
+      res
+        .json()
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err))
+    );
+  }, []);
   return (
     <div className="app">
       <Header />
