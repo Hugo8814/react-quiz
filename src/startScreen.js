afd4692355062debdc53c40ef0c778e90c1f4aea
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-04-29 21:39:08 +0100

    yep[

---
+++ src/startScreen.js
@@ -0,0 +1,11 @@
+function startScreen() {
+  return (
+    <div className="start">
+      <h2>Welcome to readyeact Quiz!</h2>
+      <h3>X questions to test your React mastery</h3>
+      <button>lets start</button>
+    </div>
+  );
+}
+
+export default startScreen;
