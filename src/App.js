f4d9f53c4ae32ee22add58ba9795e95021997b18
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-04-29 21:58:23 +0100

    wow

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./Header";
 import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
-import StartScreen from "./StartScreen";
+import StartScreen from "./StartSreen";
 
 const initialState = {
   questions: [],
@@ -25,6 +25,8 @@ function reducer(state, action) {
 
 export default function App() {
   const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
   useEffect(function () {
     fetch("http://localhost:9000/questions").then((res) =>
       res
@@ -39,7 +41,7 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
