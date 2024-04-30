f455be68a90d66795c8f8d2231167cd7694fe6b5
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-04-30 21:22:22 +0100

    yeah ik

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,25 @@ import Header from "./Header";
 import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
-import StartScreen from "./StartSreen";
+import StartScreen from "./StartScreen";
 import Question from "./Question";
 
 const initialState = {
   questions: [],
   status: "loading", //"loading", "ready", "error" "active" "finished" teo
+
+  index: 0,
 };
 
 function reducer(state, action) {
   switch (action.type) {
     case "dataReceived":
       return { ...state, questions: action.payload, status: "ready" };
-    // case "setLoading":
-    //   return { ...state, status: "loading" };
+
     case "dataFailed":
       return { ...state, status: "error" };
 
-    case "setActive":
+    case "start":
       return { ...state, status: "active" };
     default:
       throw new Error("unknown action");
@@ -28,27 +29,29 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
-
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   const numQuestions = questions.length;
   useEffect(function () {
-    fetch("http://localhost:9000/questions").then((res) =>
-      res
-        .json()
-        .then((data) => dispatch({ type: "dataReceived", payload: data }))
-        .catch((err) => dispatch({ type: "dataFailed" }))
-    );
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
+
   return (
     <div className="app">
       <Header />
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
+
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,11 +1,11 @@
 function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
-      <h2>Welcome to readyeact Quiz!</h2>
+      <h2>Welcome to React Quiz!</h2>
       <h3>{numQuestions} questions to test your React mastery</h3>
       <button
         className="btn btn-ui"
-        onClick={() => dispatch({ type: "active" })}
+        onClick={() => dispatch({ type: "start" })}
       >
         lets start
       </button>
