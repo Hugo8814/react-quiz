1eaa20c5961337aee1007deba91e4acdfc1d8f04
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-04-30 22:26:30 +0100

    teo

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ const initialState = {
   status: "loading", //"loading", "ready", "error" "active" "finished" teo
 
   index: 0,
+  answer: null,
 };
 
 function reducer(state, action) {
@@ -23,13 +24,15 @@ function reducer(state, action) {
 
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      return { ...state, answer: action.payload };
     default:
       throw new Error("unknown action");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -51,7 +54,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
